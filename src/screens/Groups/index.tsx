884ac70972a698e36b3d514c9037922237722b0b
Agacy junior <juniormartinssom@gmail.com>
2022-09-19 20:58:48 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FlatList } from 'react-native';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -5,13 +8,22 @@ import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Rocket']);
+
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     
-      <GroupCard 
-        title="Galera do Ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+      />
     </Container>
   );
 }

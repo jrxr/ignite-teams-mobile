f8d9f06515f74c52f0d5244e5421e03145850f99
Agacy junior <juniormartinssom@gmail.com>
2022-09-21 15:56:27 -0300

    Criando componente PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,6 +6,7 @@ import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
+import { PlayerCard } from "@components/PlayerCard";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
@@ -44,6 +45,14 @@ export function Players() {
 
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   );
 }

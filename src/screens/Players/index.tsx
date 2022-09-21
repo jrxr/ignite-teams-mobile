485823c1ade8e639c298ff0a59097a4e45fd7a4e
Agacy junior <juniormartinssom@gmail.com>
2022-09-21 15:43:14 -0300

    Listando os filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,36 +1,49 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
-
-import { Container, Form } from "./styles";
-import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { Input } from "@components/Input";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight 
+      <Highlight
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
 
       <Form>
-        <Input 
-          placeholder="Nome da pessoa"
-          autoCorrect={false}
-        />
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
 
-        <ButtonIcon 
-          icon="add" 
-        />
+        <ButtonIcon icon="add" />
       </Form>
 
-      <Filter 
-        title="Time A"
-        isActive
-      />
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
-  )
+  );
 }

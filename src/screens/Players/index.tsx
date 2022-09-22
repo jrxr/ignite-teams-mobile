9ed856b5050af8f3c71f44e0c93589378844503b
Agacy junior <juniormartinssom@gmail.com>
2022-09-22 19:54:53 -0300

    Passando parâmetros pela rota

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,42 +1,56 @@
-import { useState } from "react";
-import { FlatList } from "react-native";
+import { useState } from 'react';
+import { FlatList } from 'react-native'
+import { useRoute } from '@react-navigation/native';
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
-import { PlayerCard } from "@components/PlayerCard";
-import { ListEmpty } from "@components/ListEmpty";
-import { Button } from "@components/Button";
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
+type RouteParams = {
+  group: string;
+}
+
 export function Players() {
-  const [team, setTeam] = useState("Time A");
+  const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState([]);
+
+  const route = useRoute()
+
+  const { group } = route.params as RouteParams
 
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight
-        title="Nome da turma"
+      <Highlight 
+        title={group}
         subtitle="adicione a galera e separe os times"
       />
 
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <Input 
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
 
-        <ButtonIcon icon="add" />
+        <ButtonIcon 
+          icon="add" 
+        />
       </Form>
 
       <HeaderList>
-        <FlatList
-          data={["Time A", "Time B"]}
-          keyExtractor={(item) => item}
+        <FlatList 
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
           renderItem={({ item }) => (
-            <Filter
+            <Filter 
               title={item}
               isActive={item === team}
               onPress={() => setTeam(item)}
@@ -45,26 +59,31 @@ export function Players() {
           horizontal
         />
 
-        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
       </HeaderList>
 
-      <FlatList
+      <FlatList 
         data={players}
-        keyExtractor={(item) => item}
+        keyExtractor={item => item}
         renderItem={({ item }) => (
-          <PlayerCard name={item} onRemove={() => {}} />
+          <PlayerCard 
+            name={item} 
+            onRemove={() => {}}
+          />
         )}
         ListEmptyComponent={() => (
           <ListEmpty message="Não há pessoas nesse time" />
         )}
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={[
-          { paddingBottom: 100 },
-          players.length === 0 && { flex: 1 },
-        ]}
+        contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
       />
 
-      <Button title="Remover Turma" type="SECONDARY" />
+      <Button 
+        title="Remover Turma"
+        type="SECONDARY"
+      />
     </Container>
-  );
+  )
 }

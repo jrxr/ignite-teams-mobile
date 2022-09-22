01505eae7c5266d82f8f8d1a546644f4b6fcbe7b
Agacy junior <juniormartinssom@gmail.com>
2022-09-22 18:59:19 -0300

    Implementando o voltar

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native'
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -8,15 +10,14 @@ import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
 import { Container } from './styles';
-import { useNavigation } from '@react-navigation/native';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
-  const navigation = useNavigation();
+  const navigation = useNavigation()
 
   function handleNewGroup() {
-    navigation.navigate('new');
+    navigation.navigate('new')
   }
 
   return (
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -6,22 +8,35 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+
+  const navigation = useNavigation()
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' })
+  }
+
   return (
     <Container>
       <Header showBackButton />
 
       <Content>
         <Icon />
-
-        <Highlight
+        
+        <Highlight 
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input placeholder="Nome da turma" />
+        <Input
+          placeholder="Nome da turma"
+        />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button 
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handleNew}
+        />
       </Content>
     </Container>
-  );
+  )
 }
